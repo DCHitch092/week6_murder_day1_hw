a018a3c5699a8f984b131e6972c463b1f3a0ac10
Richard <fahitch+github@gmail.com>
2019-11-18 19:58:04 +0000

    second episode

---
+++ runner.js
@@ -0,0 +1,14 @@
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+// I predict that the murderer is Professor Plum
+console.log(verdict);
